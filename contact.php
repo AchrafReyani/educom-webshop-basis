b2b83ab812b991acad828a03797186b8e595f83e
Achraf Reyani <achrafreyani99@gmail.com>
2024-04-11 12:15:17 +0200

    fixed errors with variables in the contact form and placed validation logic back into the contact.php file

--- contact.php
+++ contact.php
@@ -1,5 +1,61 @@
 
 <?php 
+
+  //add empty values to variables
+  $pronouns = $name = $email = $phonenumber = $street = $housenumber = $postalcode =
+  $city = $communication = $message = "";
+  
+  $communicationmethod = array("email", "phone", "postal");
+
+  //initiate error message variables
+  $pronounError = $nameError = $emailError = $phonenumberError = $streetError = $housenumberError = $postalcodeError = $cityError = $communicationError = $messageError = $valid = "";
+
+  function validateInput() { 
+
+    //TODO remove duplicate checking where applicable
+    //TODO save valid inputs when form isn't complete after hitting send
+
+
+    //TODO make a required fields array where you can loop through these 4 fields 
+    if (empty($_POST["pronouns"])) {
+      $pronounError = "Pronouns are required";
+    } 
+
+  if (empty($_POST["name"])) {
+      $nameError = "Name is required";
+    } 
+
+  if (empty($_POST["message"])) {
+      $messageError = "Message is required";
+    } 
+
+  if (empty($_POST["communication"])) {
+      $communicationError = "Communication method is required";
+    }
+    
+
+    //TODO check to see if this can be made smaller
+  if ($_POST["communication"] == "email" && empty($_POST["email"])) {
+      $emailError = "Please enter a valid email address";
+    } else if ($_POST["communication"] == "email" && !empty($_POST["email"]) &&  !empty($_POST["pronouns"]) && !empty($_POST["name"]) && !empty($_POST["message"]) ) {
+        $valid  = true;
+    }
+
+  if ($_POST["communication"] == "phone" && empty($_POST["phonenumber"])) {
+      $phonenumberError = "Please enter a valid phone number";
+    } else if ($_POST["communication"] == "phone" && !empty($_POST["phonenumber"]) &&  !empty($_POST["pronouns"]) && !empty($_POST["name"]) && !empty($_POST["message"]))
+    {
+        $valid = true;
+    }
+
+if ($_POST["communication"] == "postal" && empty($_POST["street"]) || empty($_POST["housenumber"]) || empty($_POST["postalcode"]) || empty($_POST["city"]) || empty($_POST["communication"]) || empty($_POST["message"])) {
+    $streetError = $housenumberError = $postalcodeError = $cityError = "Please fill in the entire address"; } 
+    else if ($_POST["communication"] == "postal" && !empty($_POST["street"]) && !empty($_POST["housenumber"]) && !empty($_POST["postalcode"]) && !empty($_POST["city"]) &&  !empty($_POST["pronouns"]) && !empty($_POST["name"]) && !empty($_POST["message"])) { 
+    $valid = true;
+  }
+
+}
+
   function showFormStart() {
     echo "<h2>Contact Me</h2>
 
@@ -8,45 +64,21 @@
     ";
   }
 
-  function showFormField($fieldName, $label, $data) {
+  function showFormField($fieldName, $label, $data, $fieldNameError) {
 
     echo "
     <div>
     <label for=\"$fieldName\">$label:</label>
     <input type=\"text\" name=\"name\" value=\"$data\">
-    <span>* error message</span>
+    <span>* <?php echo $fieldNameError;?></span>
     </div>";
 
   }
-
-  function showFormEnd($returnPage, $submitButtonText) {
-    echo "<div>
-    <input type=\"submit\" value=\"Send\">
-    </div>
-  </form>";
-
-  }
-
-
-  function showContactPage(){
-    
-    showFormStart();
-    
-    showFormField("name", "Name", "name"); 
-    showFormField("email", "Email", "email"); 
-    showFormField("message", "Message", "message"); 
   
-    showFormEnd("contact.php", "Send");
-  
-
-  }
-
-  ?>
-  
-  $valid 
-
-<?php /*
-<div>
+  //temporary function to show pronoun input field
+  function showPronounField($pronouns, $pronounError) {
+    echo "
+    <div>
   <label for=\"pronouns\">Pronouns:</label>
   <select name=\"pronouns\" value=\"$pronouns\">
     <option value=\"\">Please select a pronoun</option>
@@ -58,65 +90,55 @@
   </select>
   <span>* <?php echo $pronounError;?></span>
   </div>
+    ";
+  }
 
-  <div>
-  <label for=\"name\">Name:</label>
-  <input type=\"text\" name=\"name\" value=\"$name\">
-  <span>* <?php echo $nameError;?></span>
-  </div>
-
-  <div>
-  <label for=\"email\">Email:</label>
-  <input type=\"email\" name=\"email\" value=\"$email\">
-  <span> <?php echo $emailError;?></span>
-  </div>
-
-  <div>
-  <label for=\"phone number\">Phone number:</label>
-  <input type=\"tel\" name=\"phonenumber\">
-  <span>$phonenumberError</span>
-  </div>
-
-  <div>
-  <label for=\"street\">Street name:</label>
-  <input type=\"text\" name=\"street\" value=\"$street\">
-  </div>
-
-  <div>
-  <label for=\"house number\">House number (+ suffix if applicable):</label>
-  <input type=\"text\" name=\"housenumber\" value=\"$housenumber\">
-  </div>
-
-  <div>
-  <label for=\"postal code\">Postal code:</label>
-  <input type=\"text\" name=\"postalcode\" value=\"$postalcode\">
-  </div>
-
-  <div>
-  <label for=\"city\">City:</label>
-  <input type=\"text\" name=\"city\" value=\"$city\">
-  </div>
-
-  <div>
-  <p>Preferred Communication Method:</p> <span>* $communicationError</span>
-
-  <label for=\"email-communication\">Email</label>
-  <input type=\"radio\" id=\"email-communication\" name=\"communication\" value=\"$communicationmethod[0]\">
+  //temporary function to show communication preference input field
+  function showCommunicationPreference() {
+    global $communicationError, $communicationmethod;
   
-
-  <label for=\"phone-communication\">Phone</label>
-  <input type=\"radio\" id=\"phone-communication\"  name=\"communication\" value=\"$communicationmethod[1]\">
+    echo "
+      <div>
+        <p>Preferred Communication Method:</p> <span>* $communicationError</span>
+    ";
   
-  <label for=\"postal-communication\">Postal Mail</label>
-  <input type=\"radio\" id=\"postal-communication\" name=\"communication\" value=\"$communicationmethod[2]>
-  </div>
+    foreach ($communicationmethod as $method) {
+      $methodId = lcfirst($method) . "-communication"; // Generate unique ID for each radio button
+      echo "
+        <label for=\"$methodId\">$method</label>
+        <input type=\"radio\" id=\"$methodId\" name=\"communication\" value=\"$method\">
+      ";
+    }
+  
+    echo "</div>";
+  }
 
-  <div>
-  <label for=\"message\">Message:</label>
-  <textarea name=\"message\"></textarea>
-  <span>* $messageError </span>
-  </div>
+  function showFormEnd() {
+    echo "<div>
+    <input type=\"submit\" value=\"Send\">
+    </div>
+  </form>";
 
-*/?>
+  }
+
+
+  function showContactPage(){ //TODO fix the contact form so that it submits again
+    
+    showFormStart();
+    showPronounField("pronouns", "pronounError");//TODO
+    showFormField("name", "Name", "", "nameError"); 
+    showFormField("email", "Email", "", "emailError"); 
+    showFormField("phonenumber", "Phone number", "", "phonenumberError");
+    showFormField("street", "Street", "", "streetError"); 
+    showFormField("housenumber", "House number", "", "housenumberError");
+    showFormField("postalcode", "Postal code", "", "postalcodeError");
+    showFormField("city", "City", "", "cityError"); 
+    showCommunicationPreference();//TODO
+    showFormField("message", "Message", "", "messageError"); 
+    showFormEnd();
+  
+  }
+
+  ?>
 
  
--- index.php
+++ index.php
@@ -11,60 +11,6 @@ include 'thankyou.php';
 include 'beginDocument.php';
 include 'endDocument.php';
 
-  //initiate variables global
-  global $valid;
-  global $pronouns, $name, $email, $phonenumber, $street, $housenumber, $postalcode, $city, $communication, $message;
-  global $communicationmethod;
-  global $pronounError, $nameError, $emailError, $phonenumberError, $streetError, $housenumberError, $postalcodeError, $cityError, $communicationError, $messageError, $valid;
-  
-  //add empty values to variables
-  $pronouns = $name = $email = $phonenumber = $street = $housenumber = $postalcode =
-  $city = $communication = $message = "";
-  
-  $communicationmethod = ["email", "phone", "postal"];
-
-  //initiate error message variables
-  $pronounError = $nameError = $emailError = $phonenumberError = $streetError = $housenumberError = $postalcodeError = $cityError = $communicationError = $messageError = $valid = "";
-
-//functions
-
-function validateInput() {
-    if (empty($_POST["pronouns"])) {
-      $pronounError = "Pronouns are required";
-    } 
-
-  if (empty($_POST["name"])) {
-      $nameError = "Name is required";
-    } 
-
-  if (empty($_POST["message"])) {
-      $messageError = "Message is required";
-    } 
-
-  if (empty($_POST["communication"])) {
-      $communicationError = "Communication method is required";
-    }
-
-  if ($_POST["communication"] == "email" && empty($_POST["email"])) {
-      $emailError = "Please enter a valid email address";
-    } else if ($_POST["communication"] == "email" && !empty($_POST["email"]) &&  !empty($_POST["pronouns"]) && !empty($_POST["name"]) && !empty($_POST["message"]) ) {
-        $valid  = true;
-    }
-
-  if ($_POST["communication"] == "phone" && empty($_POST["phonenumber"])) {
-      $phonenumberError = "Please enter a valid phone number";
-    } else if ($_POST["communication"] == "phone" && !empty($_POST["phonenumber"]) &&  !empty($_POST["pronouns"]) && !empty($_POST["name"]) && !empty($_POST["message"]))
-    {
-        $valid = true;
-    }
-
-if ($_POST["communication"] == "postal" && empty($_POST["street"]) || empty($_POST["housenumber"]) || empty($_POST["postalcode"]) || empty($_POST["city"]) || empty($_POST["communication"]) || empty($_POST["message"])) {
-    $streetError = $housenumberError = $postalcodeError = $cityError = "Please fill in the entire address"; } 
-    else if ($_POST["communication"] == "postal" && !empty($_POST["street"]) && !empty($_POST["housenumber"]) && !empty($_POST["postalcode"]) && !empty($_POST["city"]) &&  !empty($_POST["pronouns"]) && !empty($_POST["name"]) && !empty($_POST["message"])) { 
-    $valid = true;
-  }
-
-}
 
 function showContent($data){
 	switch($data)
@@ -77,6 +23,9 @@ function showContent($data){
 		  break;
 		case 'Contact';
 		  showContactPage();
+      break;
+    case 'Thankyou';
+      showThankYouPage();
 		  break;
 		default; 
 		  showHomePage();
@@ -86,8 +35,10 @@ function showContent($data){
 $requestedPage = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $requestedPage = trim($requestedPage, '/');  // Remove leading/trailing slashes
 
-function getRequestedPage() {
-	// Check for page in POST data if request method is POST
+function getRequestedPage() { //TODO check back to see if this is single door routing or not
+	$page = 'Home';
+  
+  // Check for page in POST data if request method is POST
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	  if (isset($_POST['page'])) {
 		return $_POST['page'];
@@ -115,8 +66,6 @@ function showResponsePage($data){
 	showFooter();
 	endDocument();
 }
-
-
 
 $page = getRequestedPage();
 showResponsePage($page);
